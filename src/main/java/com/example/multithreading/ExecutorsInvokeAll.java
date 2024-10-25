dd83c7a2339ac62d35bb10b434b65b702b61ec7c
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-10-25 12:08:04 +0530

    initial commit

--- src/main/java/com/example/multithreading/ExecutorsInvokeAll.java
+++ src/main/java/com/example/multithreading/ExecutorsInvokeAll.java
@@ -1,50 +1,45 @@
 package com.example.multithreading;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class ExecutorsInvokeAll {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " Started");
+        ExecutorService executor = Executors.newFixedThreadPool(3); // Create a fixed thread pool with 3 threads
+        List<Callable<Integer>> callables = new ArrayList<>();
+
+        for (int i = 1; i <= 20; i++) {
+            final Integer num = i;
+            callables.add(() -> {
+                sleep((int) (Math.random() * 1000));
+                System.out.println(Thread.currentThread().getName() + ": " + num);
+                return num;
+            });
+        }
+
+        List<Future<Integer>> results = executor.invokeAll(callables);
+
+        for (Future<Integer> future : results) {
+            try {
+                Integer i = future.get();// Wait for task to complete before submitting the next task
+                System.out.println(Thread.currentThread().getName() + " === " + i);
+            } catch (InterruptedException | ExecutionException e) {
+                System.out.println("Exception: " + e.getMessage());
+            }
+        }
+
+        executor.shutdown(); // Shutdown the executor
+        System.out.println(Thread.currentThread().getName() + " Finished");
+    }
 
     private static void sleep(int millis) {
         try {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Exception: " + e.getMessage());
         }
-    }
-
-    private static String algoOne() {
-        System.out.println("Thread : " + Thread.currentThread().getName());
-        System.out.println("Running algorithm one");
-        sleep(1000);
-        return "algoOne Result";
-    }
-
-    private static String algoTwo() {
-        System.out.println("Thread : " + Thread.currentThread().getName());
-        System.out.println("Running algorithm two");
-        sleep(1000);
-        return "algoTwo Result";
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-
-        List<Future<String>> taskFutures = executorService.invokeAll(Arrays.asList(ExecutorsInvokeAll::algoOne, ExecutorsInvokeAll::algoTwo));
-
-        taskFutures.forEach(res -> {
-            try {
-                System.out.println("Result - " + res.get());
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-        });
-
-        executorService.shutdown();
     }
 }
