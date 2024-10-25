a4a0f973d14c408aaf006acdc6170e95bda88736
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-10-25 12:54:50 +0530

    initial commit

--- src/main/java/com/example/multithreading/ExecutorsInvokeAll.java
+++ src/main/java/com/example/multithreading/ExecutorsInvokeAll.java
@@ -19,20 +19,22 @@ public class ExecutorsInvokeAll {
                 return num;
             });
         }
-
-        List<Future<Integer>> results = executor.invokeAll(callables);
-
-        for (Future<Integer> future : results) {
-            try {
-                Integer i = future.get();// Wait for task to complete before submitting the next task
-                System.out.println(Thread.currentThread().getName() + " === " + i);
-            } catch (InterruptedException | ExecutionException e) {
-                System.out.println("Exception: " + e.getMessage());
+        try {
+            List<Future<Integer>> results = executor.invokeAll(callables);
+            for (Future<Integer> future : results) {
+                try {
+                    Integer i = future.get();// Wait for task to complete before submitting the next task
+                    System.out.println(Thread.currentThread().getName() + " === " + i);
+                } catch (InterruptedException | ExecutionException e) {
+                    System.out.println("Exception: " + e.getMessage());
+                }
             }
+            System.out.println(Thread.currentThread().getName() + " Finished");
+        } catch (InterruptedException e) {
+            System.out.println("Exception: " + e.getMessage());
+        } finally {
+            shutdownAndAwaitTermination(executor);
         }
-
-        executor.shutdown(); // Shutdown the executor
-        System.out.println(Thread.currentThread().getName() + " Finished");
     }
 
     private static void sleep(int millis) {
@@ -40,6 +42,26 @@ public class ExecutorsInvokeAll {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
             System.out.println("Exception: " + e.getMessage());
+        }
+    }
+
+    private static void shutdownAndAwaitTermination(ExecutorService pool) {
+        // Disable new tasks from being submitted
+        pool.shutdown();
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                // Cancel currently executing tasks forcefully
+                pool.shutdownNow();
+                // Wait a while for tasks to respond to being cancelled
+                if (!pool.awaitTermination(60, TimeUnit.SECONDS))
+                    System.err.println("Pool did not terminate");
+            }
+        } catch (InterruptedException ex) {
+            // (Re-)Cancel if current thread also interrupted
+            pool.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
         }
     }
 }
