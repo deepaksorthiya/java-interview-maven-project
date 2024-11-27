7f00375ef2174536acd3dd8ebf5b1af3d51fd996
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-11-27 10:26:42 +0530

    initial commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,15 @@
 package com.example;
 
-public class App {
-    public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("String msg {}", Arrays.toString(args));
     }
 
 }
